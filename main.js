96e8996c41122f3bfe3da3453d56c46997baca48
gitanik <37584526+gitanik@users.noreply.github.com>
2018-04-03 17:04:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {
+	$(window).scrollTop();
+	
+	if($(this).scrollTop() > 750) {
+		console.log(">135");
+		$(".nav-menu").addClass("fixed");
+		$(".white-above").addClass("fixed1");
+	}else{
+		$(".nav-menu").removeClass("fixed");
+		$(".white-above").removeClass("fixed1");
+	}
+});
+
